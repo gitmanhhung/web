7f60e0269f3cf4fab7f04a2e8d03c0a7dda6f74f
Duong Manh Hung <thpt10dmanhhung@gmail.com>
2020-12-31 15:56:42 +0700

    lan1

---
+++ WEB_datphong/WEB_datphong/Areas/Admin/Controllers/PhongAdminController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEB_datphong.Controllers;
+using WEB_datphong.Models.BUS;
+
+namespace WEB_datphong.Areas.Admin.Controllers
+{
+    public class PhongAdminController : Controller
+    {
+        // GET: Admin/PhongAdmin
+        public ActionResult Index()
+        {
+            var db = HotelOnlineClass.DanhSachPhong();
+            return View(db);
+        }
+
+        // GET: Admin/PhongAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/PhongAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/PhongAdmin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/PhongAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/PhongAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/PhongAdmin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/PhongAdmin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WEB_datphong/WEB_datphong/Models/BUS/HotelOnlineClass.cs
@@ -0,0 +1,34 @@
+﻿using HotelConection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace WEB_datphong.Models.BUS
+{
+    public class HotelOnlineClass
+    {
+        public static IEnumerable<PHONG> DanhSach()
+        {
+            var db = new HotelConectionDB();
+            return db.Query<PHONG>("select * from PHONG where TinhTrang = 0");
+        }
+        public static PHONG ChiTiet(String a)
+        {
+            var db = new HotelConectionDB();
+            return db.SingleOrDefault<PHONG>("select * from PHONG where MaPhong=@0",a);
+        }
+        public static IEnumerable<PHONG> newPhong()
+        {
+            var db = new HotelConectionDB();
+            return db.Query<PHONG>("select * from PHONG where GhiChu = N'new'");
+        }
+        public static IEnumerable<PHONG> DanhSachPhong()
+        {
+            var db = new HotelConectionDB();
+            return db.Query<PHONG>("select * from PHONG");
+        }
+
+    }
+}
